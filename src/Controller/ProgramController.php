8fee793092c124929d8e075c3ab0c597ec9fe2ea
maeva.pllt@gmail.com <maeva.pllt@gmail.com>
2020-12-06 21:29:11 +0100

    quest 5

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -1,11 +1,14 @@
 <?php
-// src/Controller/ProgramController.php
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @return Response
+ */
 Class ProgramController extends AbstractController
 {
     /**
@@ -16,5 +19,17 @@ Class ProgramController extends AbstractController
         return $this->render('program/index.html.twig', [
             'website' => 'Wild Séries',
         ]);
+    }
+
+    /**
+     * @Route("/programs/{id}", requirements={"id"="\d+"}, methods={"GET"}, name="program_show")
+     * @param int $id
+     * @return Response
+     */
+    public function show(int $id): Response
+    {
+            return $this->render('program/show.html.twig', [
+                'id' => $id,
+            ]);
     }
 }
